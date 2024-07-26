a3b81e1341a0a88544e64d00298167761ee2f2fb
maxxxkam <muslimovmaxkam@gmail.com>
2024-07-26 11:32:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let b = prompt('сколько вам лет?')
+
+ if (b < 0) {
+    console.log('тебя еще в планах нет');
+}else if (b == 0) {
+    console.log('ты не родился');
+}else if (b < 18) {
+    console.log('маленки');
+} else if (b > 18) {
+    console.log('тебе можно');
+}
+
+
+
+
+
+
